4aa6611afd5147184919a9ce62cb69d4890eda60
Marthijs Berfelo <marthijs.berfelo@hybrit.org>
2020-03-06 11:37:08 +0100

    initial setup

---
+++ src/test/kotlin/org/hybrit/uno/api/cars/CarsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.hybrit.uno.api.cars
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarsApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
